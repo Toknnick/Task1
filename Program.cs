510e94df05ed6f5539b0b5c6d49930cf951ef2bd
Maksim <maksimus.tumannick@yandex.ru>
2022-06-20 19:01:27 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte small;
+            short distance;
+            int number;
+            long bigNumber;
+            decimal power;
+            char simbol;
+            string name;
+            float divided;
+            double dividedOfNumber;
+            bool isWork;
+        }
+    }
+}
